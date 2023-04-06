ec7fd10a78e7de09e0dbd1b1976e635918d2c88b
devriimd8 <125180874+devriimd8@users.noreply.github.com>
2023-04-06 13:49:24 +0300

    Add files via upload

---
+++ KdvHesaplama.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class KdvHesaplama {
+    public static void main(String[] args) {
+        /* (Not : KDV tutarını 18% olarak alın)
+KDV'siz Fiyat = 10;
+KDV'li Fiyat = 11.8;
+KDV tutarı = 1.8;
+Eğer girilen tutar 0 ve 1000 TL arasında ise KDV oranı %18 ,
+tutar 1000 TL'den büyük ise KDV oranını %8 olarak KDV tutarı hesaplayan programı yazınız.*/
+        Scanner input = new Scanner(System.in);
+        double fiyat, kdvOran, kdvFiyat, kdvliFiyat;
+        System.out.println("ücret tutarını giriniz :");
+        fiyat = input.nextInt();
+        kdvOran = (fiyat>0) && (fiyat<1000) ? 0.18 : 0.8;
+        kdvFiyat = fiyat * kdvOran;
+        kdvliFiyat = fiyat + kdvFiyat;
+
+        System.out.println("KDV oranı : " + kdvOran);
+        System.out.println("KDV Fiyatı: " + kdvFiyat);
+        System.out.println("KDV'li Fiyat: " + kdvliFiyat);
+    }
+}
